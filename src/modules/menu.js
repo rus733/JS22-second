4c0b7480c9228fae9a63b4f1566fb4ca1fea0eaa
rus733 <9282716617@mail.ru>
2021-12-26 15:37:53 +0300

    упростили вызов a закрытия по клику , убрав промежу

---
+++ src/modules/menu.js
@@ -0,0 +1,27 @@
+const menu = () => {
+  const menuBtn = document.querySelector('.menu');
+  const menu = document.querySelector('menu');
+  const closeBtn = menu.querySelector('.close-btn');
+  const menuItems = menu.querySelectorAll('ul>li>a');
+  console.log(menuItems);
+
+  // ф  закрытия меню
+  const handleMenu = () => {
+    if (!menu.style.transform) {
+      menu.style.transform = 'translateX(0)';
+    } else {
+      menu.style.transform = '';
+    }
+  };
+
+  //кнопка меню
+  menuBtn.addEventListener('click', handleMenu());
+  //кнопка закрытия меню
+  closeBtn.addEventListener('click', handleMenu());
+  //перебор ссылок меню и закрытие меню
+  for (let i = 0; i < menuItems.length; i++) {
+    menuItems[i].addEventListener('click', handleMenu());
+  }
+};
+
+export default menu;
