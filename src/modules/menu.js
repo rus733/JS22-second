8c0a69bad59a5f070d87c0a474a8eacfef88a9f6
rus733 <9282716617@mail.ru>
2021-12-26 21:42:36 +0300

    убрал лишний код

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -2,21 +2,12 @@ const menu = () => {
   const menuBtn = document.querySelector('.menu');
   const menu = document.querySelector('menu');
   const closeBtn = menu.querySelector('.close-btn');
-  //const menuItems = menu.querySelectorAll('ul>li>a');
-
   const menuItem = menu.querySelectorAll('ul>li>a[href*="#"]');
   const mainBlock = document.querySelector('main');
   const mainLink = mainBlock.querySelector('a[href*="#"]');
 
   // ф  закрытия меню
   const handleMenu = () => {
-    /*active-menu
-    if (!menu.style.transform) {
-      menu.style.transform = 'translateX(0)';
-    } else {
-      menu.style.transform = '';
-    }
-    */
     menu.classList.toggle('active-menu');
   };
 
@@ -25,11 +16,7 @@ const menu = () => {
   //кнопка закрытия меню
   closeBtn.addEventListener('click', handleMenu);
 
-  //перебор элементов меню бургер 1 вар
-
-  //menuItems.forEach((menuItem) => menuItem.addEventListener('click', /////handleMenu));
-
-  //перебор элементов меню бургер 2 вар
+  //перебор элементов меню бургер
 
   menuItem.forEach((item) => {
     item.addEventListener('click', (e) => {
