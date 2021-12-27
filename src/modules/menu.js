93b325a29b98fdd6509739d8b5c6e2070102e513
rus733 <9282716617@mail.ru>
2021-12-27 09:00:33 +0300

    введена ф. scrollScreen - плавная прокрутка

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -5,38 +5,46 @@ const menu = () => {
   const menuItem = menu.querySelectorAll('ul>li>a[href*="#"]');
   const mainBlock = document.querySelector('main');
   const mainLink = mainBlock.querySelector('a[href*="#"]');
-
   // ф  закрытия меню
   const handleMenu = () => {
     menu.classList.toggle('active-menu');
   };
 
-  //кнопка меню
-  menuBtn.addEventListener('click', handleMenu);
-  //кнопка закрытия меню
-  closeBtn.addEventListener('click', handleMenu);
-
-  //перебор элементов меню бургер
-
-  menuItem.forEach((item) => {
-    item.addEventListener('click', (e) => {
-      e.preventDefault();
-      const blockId = item.getAttribute('href');
-      document.querySelector(blockId).scrollIntoView({
-        behavior: 'smooth',
-        block: 'start',
-      });
-      handleMenu();
-    });
-  });
-
-  mainLink.addEventListener('click', (e) => {
-    e.preventDefault();
-    const blockId = mainLink.getAttribute('href');
+  const scrollScreen = (element) => {
+    const blockId = element.getAttribute('href');
     document.querySelector('' + blockId).scrollIntoView({
       behavior: 'smooth',
       block: 'start',
     });
+  };
+
+  //перебор элементов меню бургер
+  menuItem.forEach((item) => {
+    item.addEventListener('click', (e) => {
+      e.preventDefault();
+      scrollScreen(item);
+      //const blockId = item.getAttribute('href');
+      //document.querySelector(blockId).scrollIntoView({
+      //behavior: 'smooth',
+      // block: 'start',
+      //});
+      handleMenu();
+    });
+  });
+
+  //кнопка меню
+  menuBtn.addEventListener('click', handleMenu);
+  //кнопка закрытия меню
+  closeBtn.addEventListener('click', handleMenu);
+  // кнопка прокрутки
+  mainLink.addEventListener('click', (e) => {
+    e.preventDefault();
+    scrollScreen(mainLink);
+    //const blockId = mainLink.getAttribute('href');
+    //document.querySelector('' + blockId).scrollIntoView({
+    //behavior: 'smooth',
+    //block: 'start',
+    //});
   });
 };
 
--- src/modules/modal.js
+++ src/modules/modal.js
@@ -2,7 +2,24 @@ const modal = () => {
   const modal = document.querySelector('.popup');
   const buttons = document.querySelectorAll('.popup-btn');
   const closeBtn = modal.querySelector('.popup-close');
+  // переменные для анимации
+  let opacityValue = 0,
+    step = 0.03,
+    animInterval = 0;
 
+  // анимация
+  function animModalWindow() {
+    opacityValue += step;
+
+    if (opacityValue >= 1) {
+      opacityValue = 1;
+      cancelAnimationFrame(animInterval);
+    }
+    modal.style.opacity = opacityValue;
+    animInterval = requestAnimationFrame(animModalWindow);
+  }
+
+  // открываем модальное окно
   buttons.forEach((btn) => {
     btn.addEventListener('click', () => {
       // проверка размера экрана
@@ -15,23 +32,7 @@ const modal = () => {
       }
     });
   });
-
-  // анимация
-  let opacityValue = 0,
-    step = 0.03,
-    animInterval = 0;
-
-  function animModalWindow() {
-    opacityValue += step;
-
-    if (opacityValue >= 1) {
-      opacityValue = 1;
-      cancelAnimationFrame(animInterval);
-    }
-    modal.style.opacity = opacityValue;
-    animInterval = requestAnimationFrame(animModalWindow);
-  }
-
+  // закрываем модальное окно
   closeBtn.addEventListener('click', () => {
     modal.style.display = 'none';
     opacityValue = 0;
