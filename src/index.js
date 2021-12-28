27548a8b3fed0077c118151c76cc31c0712693c8
rus733 <9282716617@mail.ru>
2021-12-28 19:35:51 +0300

    задание 21

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@
 import timer from './modules/timer';
 import menu from './modules/menu';
 import modal from './modules/modal';
+import regex from './modules/regex';
 timer('31 december 2021');
 menu();
 modal();
+regex();
