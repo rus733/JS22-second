8ae67304cd853bd0d3b9abce0d125d6d36eb761a
rus733 <9282716617@mail.ru>
2022-01-05 20:34:22 +0300

    задание 22 в отдельном файле

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -1,40 +1,37 @@
 const menu = () => {
-  const menuBtn = document.querySelector('.menu');
   const menu = document.querySelector('menu');
-  const closeBtn = menu.querySelector('.close-btn');
-  const menuItem = menu.querySelectorAll('ul>li>a[href*="#"]');
   const mainBlock = document.querySelector('main');
-  const mainLink = mainBlock.querySelector('a[href*="#"]');
-  const scrollScreen = (element) => {
-    const blockId = element.getAttribute('href');
-    document.querySelector('' + blockId).scrollIntoView({
-      behavior: 'smooth',
-      block: 'start',
-    });
-  };
-  // ф  закрытия меню
-  const handleMenu = () => {
+
+  const heandlMenu = () => {
     menu.classList.toggle('active-menu');
   };
 
-  //перебор элементов меню бургер
-  menuItem.forEach((item) => {
-    item.addEventListener('click', (e) => {
+  document.body.addEventListener('click', (e) => {
+    let target = e.target;
+    let itsMenu = target == menu || menu.contains(target);
+    let menuActive = menu.classList.contains('active-menu');
+    if (target.closest('.menu')) {
+      heandlMenu();
+    } else if (target.closest('.close-btn')) {
+      heandlMenu();
+    } else if (target.closest('a[href*="#"]') && mainBlock.contains(target)) {
       e.preventDefault();
-      scrollScreen(item);
-      handleMenu();
-    });
-  });
-
-  //кнопка меню
-  menuBtn.addEventListener('click', handleMenu);
-  //кнопка закрытия меню
-  closeBtn.addEventListener('click', handleMenu);
-  // кнопка прокрутки
-  mainLink.addEventListener('click', (e) => {
-    e.preventDefault();
-    scrollScreen(mainLink);
+      const blockId = target.closest('a[href*="#"]').getAttribute('href');
+      document.querySelector(blockId).scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+      });
+    } else if (target.closest('a[href*="#"]') && menu.contains(target)) {
+      e.preventDefault();
+      const blockId = target.getAttribute('href');
+      document.querySelector(blockId).scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+      });
+      heandlMenu();
+    } else if (!itsMenu && menuActive) {
+      heandlMenu();
+    }
   });
 };
-
 export default menu;
