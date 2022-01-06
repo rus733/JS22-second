187f3717e3d876fae2ac04542c00b5f28614e4af
rus733 <9282716617@mail.ru>
2022-01-06 19:06:58 +0300

    поиск плавной прокрутки

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -21,16 +21,27 @@ const menu = () => {
     if (target.closest('.menu')) {
       toggleMenu();
       return;
-    } else if ((!itsMenu && menuActive) || target.closest('.close-btn')) {
+    }
+
+    if ((!itsMenu && menuActive) || target.closest('.close-btn')) {
       menu.classList.remove('active-menu');
       return;
-      //кнопка-ссылка в main к блоку услуг
-    } else if (target.closest('a[href*="#"]') && mainBlock.contains(target)) {
+    }
+
+    //кнопка-ссылка в main к блоку услуг
+    if (target.closest('a[href*="#"]') && mainBlock.contains(target)) {
       e.preventDefault();
       const blockId = target.closest('a[href*="#"]').getAttribute('href');
-      scrollView(blockId);
-      //кнопки-ссылки в menu
-    } else if (target.closest('a[href*="#"]') && menu.contains(target)) {
+      //scrollView(blockId);
+      document.querySelector(blockId).scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+      });
+      toggleMenu();
+    }
+
+    //кнопки-ссылки в menu
+    if (target.closest('a[href*="#"]') && menu.contains(target)) {
       e.preventDefault();
       const blockId = target.getAttribute('href');
       scrollView(blockId);
