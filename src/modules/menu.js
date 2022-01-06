4d587662227674ac6eac38ccbf1182c9d6b5ce1f
rus733 <9282716617@mail.ru>
2022-01-06 14:27:29 +0300

    удалим лишний код

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -21,13 +21,7 @@ const menu = () => {
     if (target.closest('.menu')) {
       toggleMenu();
       return;
-      //закрыл меню кнопкой
-      //} else if (target.closest('.close-btn')) {
-      //toggleMenu();
-      //console.log('закрыл меню кнопкой');
-      // return;
     } else if ((!itsMenu && menuActive) || target.closest('.close-btn')) {
-      //toggleMenu();
       menu.classList.remove('active-menu');
       return;
       //кнопка-ссылка в main к блоку услуг
@@ -42,23 +36,6 @@ const menu = () => {
       scrollView(blockId);
       toggleMenu();
     }
-
-    /*
-      //кнопка-ссылка в main к блоку услуг
-    } else if (target.closest('a[href*="#"]') && mainBlock.contains(target)) {
-      e.preventDefault();
-      const blockId = target.closest('a[href*="#"]').getAttribute('href');
-      scrollView(blockId);
-      //кнопки-ссылки в menu
-    } else if (target.closest('a[href*="#"]') && menu.contains(target)) {
-      e.preventDefault();
-      const blockId = target.getAttribute('href');
-      scrollView(blockId);
-      toggleMenu();
-    } else if (!itsMenu && menuActive || target.closest('.close-btn')) {
-      toggleMenu();
-    }
-*/
   });
 };
 export default menu;
