ec0be98c1553f30d8453cd15da6389998f7dea64
rus733 <9282716617@mail.ru>
2022-01-07 12:49:15 +0300

    menu is ready

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -1,11 +1,5 @@
 const menu = () => {
   const menu = document.querySelector('menu');
-  //const mainBlock = document.querySelector('main');
-
-  //const toggleMenu = () => {
-  //menu.classList.toggle('active-menu');
-  //};
-
   const scrollView = (id) => {
     document.querySelector(id).scrollIntoView({
       behavior: 'smooth',
@@ -17,8 +11,6 @@ const menu = () => {
     const target = e.target;
     const closestLink = target.closest('a[href*="#"]');
     const closestMenu = target.closest('menu');
-    //const itsMenu = target == menu || menu.contains(target);
-    //const menuActive = menu.classList.contains('active-menu');
 
     if (target.closest('.menu') || target.closest('.close-btn')) {
       menu.classList.toggle('active-menu');
@@ -30,7 +22,6 @@ const menu = () => {
     //скрол кнопка-ссылка в main или кнопка-ссылка в меню
     if (closestLink && (closestLink.closest('main') || closestMenu)) {
       e.preventDefault();
-      //const blockId = closestLink.getAttribute('href');
       scrollView(closestLink.getAttribute('href'));
     }
   });
