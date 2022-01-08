84dd3312c4716be526167ce65824ab3f8528326e
rus733 <9282716617@mail.ru>
2022-01-08 22:11:12 +0300

    create  variables and countCalc()

--- dist/js/main.js
+++ dist/js/main.js
@@ -26,7 +26,7 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _mod
   \*************************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\nconst calc = price => {};\r\n\r\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (calc);\r\n\n\n//# sourceURL=webpack:///./modules/calc.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\nconst calc = (price = 100) => {\r\n  const calcBlock = document.querySelector('.calc-block');\r\n  const calcType = document.querySelector('.calc-type');\r\n  const calcSquare = document.querySelector('.calc-square');\r\n  const calcCount = document.querySelector('.calc-count');\r\n  const calcDay = document.querySelector('.calc-day');\r\n  const total = document.querySelector('#total');\r\n\r\n  const countCalc = () => {\r\n    //const calcTypeValue = calcType;\r\n    console.dir(calcType.options[calcType.selectedIndex]);\r\n  };\r\n\r\n  calcBlock.addEventListener('input', e => {\r\n    //console.log(e.target);\r\n    if (e.target === calcType || e.target === calcSquare || e.target === calcCount || e.target === calcDay) {\r\n      countCalc();\r\n      //console.log(e.target);\r\n    }\r\n  });\r\n};\r\n\r\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (calc);\r\n\n\n//# sourceURL=webpack:///./modules/calc.js?");
 
 /***/ }),
 
--- src/modules/calc.js
+++ src/modules/calc.js
@@ -1,5 +1,23 @@
 const calc = (price = 100) => {
-  console.log(price);
+  const calcBlock = document.querySelector('.calc-block');
+  const calcType = document.querySelector('.calc-type');
+  const calcSquare = document.querySelector('.calc-square');
+  const calcCount = document.querySelector('.calc-count');
+  const calcDay = document.querySelector('.calc-day');
+  const total = document.querySelector('#total');
+
+  const countCalc = () => {
+    //const calcTypeValue = calcType;
+    console.dir(calcType.options[calcType.selectedIndex]);
+  };
+
+  calcBlock.addEventListener('input', e => {
+    //console.log(e.target);
+    if (e.target === calcType || e.target === calcSquare || e.target === calcCount || e.target === calcDay) {
+      countCalc();
+      //console.log(e.target);
+    }
+  });
 };
 
 export default calc;
