313a5143b58eaccbe74dd7602201a64783646ee5
rus733 <9282716617@mail.ru>
2022-01-11 13:42:21 +0300

    fix: run slider

--- src/index.js
+++ src/index.js
@@ -5,13 +5,13 @@ import validationForms from './modules/validationForms';
 import tabs from './modules/tabs';
 import slider from './modules/slider';
 import calc from './modules/calc';
+
 timer('11 january 2022');
 menu();
 modal();
 validationForms();
 tabs();
 calc(100);
-sendForm;
 slider({
   sliderContent: 'portfolio-content',
   sliderItemClass: 'portfolio-item',
