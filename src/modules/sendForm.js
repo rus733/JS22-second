e7f250fd4da9341c1bde6d516c392c9c405026ec
rus733 <9282716617@mail.ru>
2022-01-11 15:52:37 +0300

    send  data with formBody[key] = val

--- src/modules/sendForm.js
+++ src/modules/sendForm.js
@@ -10,10 +10,12 @@ const sendForm = (idForm) => {
   const sendData = (data) =>
     fetch('https://jsonplaceholder.typicode.com/posts', {
       method: 'POST',
-      body: data, // переведем data в строчку
+      ///body: data, // переведем data в строчку
       //body: JSON.stringify(data), // formData не можем превращать в JSON
+      body: JSON.stringify(data), // при отправке formBody заворачиваем в data в json'
       headers: {
-        'Content-Type': 'multipart/form-data', // при этих условиях не заварачивая в json формат
+        //'Content-Type': 'multipart/form-data', // при этих условиях не заварачивая в json формат
+        'Content-Type': 'application/json', // при отправке formBody заворачиваем в 'application/json'
       },
     }).then((res) => res.json());
 
@@ -22,9 +24,18 @@ const sendForm = (idForm) => {
 
     const formData = new FormData(form); // собирает данные со всех элементов формы
     //но только у тех у кого есть аттрибут name="user_form"
-    console.log('submit');
 
-    sendData(formData).then((data) => {
+    // соберем данные из new FormData(form)
+    // создадим пустой обьект и заполним его forEach
+    const formBody = {};
+    formData.forEach((val, key) => {
+      formBody[key] = val;
+    });
+
+    //console.log('submit');
+
+    sendData(formBody).then((data) => {
+      // и отправляем formBody
       console.log(data);
     });
   });
